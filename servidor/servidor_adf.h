ef31aa937e4f2df3203bf7c1bdd143eec5437979
jonArzelus <kontrabass32@hotmail.com>
2017-09-28 13:54:48 +0200

    trabajo

--- servidor/servidor_adf.h
+++ servidor/servidor_adf.h
@@ -5,8 +5,16 @@ Sistemas Distribuidos 2017/18
 
 //definicion de valores estandar para la ejecucion
 #define MAX_BUF 1024
-#define PORT 6800
-#define FILES_PATH	"files"
+//#define SOCK_ADDR 127.0.0.1
+#define PORT 50005
+#define FILES_PATH	"archivos"
+
+//definicion de estados posibles del programa
+#define ST_INIT	0
+#define ST_AUTH	1
+#define ST_MAIN	2
+#define ST_DOWN	3
+#define ST_UP	4
 
 //definicion de numeros de comando de usuario
 #define COM_USER	0
@@ -21,10 +29,14 @@ Sistemas Distribuidos 2017/18
 
 //definicion de valores de la aplicacion
 #define MAX_UPLOAD_SIZE	10*1024*1024 //10MB
-#define SPACE_MARGIN 50*1024*1024 //50MB
+#define SPACE_MARGIN 	50*1024*1024 //50MB
 
 //definicion del array de comandos, usuarios...
 char * COMANDOS[] = {"USER","PASS","LIST","DOWN","DOW2","UPLO","UPL2","DELE","EXIT",NULL};
 char * lista_usuarios[] = {"anonimo","user1","user2",NULL};
-char * lista_contraseñas[] = {"","user1","user2"};
+char * lista_contrasenas[] = {"","user1","user2"}; //falta NULL ?
 int estado;
+
+//crear las funciones del programa
+void sesion(int sock_conv);
+void inesperado(int s);
